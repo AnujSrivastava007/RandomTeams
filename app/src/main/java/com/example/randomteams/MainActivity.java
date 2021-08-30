c62ab92176451d9bd82a4bd267a97979bd00d3a0
Shubham Pandey <shubhampandeylearning@gmail.com>
2021-08-30 21:41:06 +0530

    Added intents, added new activities and minor UI fixes

--- app/src/main/java/com/example/randomteams/MainActivity.java
+++ app/src/main/java/com/example/randomteams/MainActivity.java
@@ -2,13 +2,45 @@ package com.example.randomteams;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    private LinearLayout layout1, layout2;
+    private Intent i;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        layout1 = findViewById(R.id.ll1);
+        layout2 = findViewById(R.id.ll2);
+
+        layout1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                manualActivity();
+            }
+        });
+
+        layout2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                importFileActivity();
+            }
+        });
+    }
+
+    private void importFileActivity() {
+        i = new Intent(this, importFile.class);
+        startActivity(i);
+    }
+
+    private void manualActivity() {
+        i = new Intent(this, manual.class);
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/com/example/randomteams/manual.java
@@ -0,0 +1,20 @@
+package com.example.randomteams;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+
+public class manual extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manual);
+
+//        ArrayAdapter<String> a = new ArrayAdapter<>(this, )
+    }
+}
